a5454f3d6fd3a381dc4feea968c7bb5506a5f1ba
Samir Azimov <samirazimov488@gmail.com>
2026-09-02 23:58:49 +0500

    fixed bugs

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/users")
 def get_users():
-    return  {"users": "Ryan, st"}
+    return  {"users": "Ryan , Azimov"}
 
 @app.get("/status")
 def get_users():
-    return  {"status": "http 201 ok"}
+    return  {"status": "http 200 ok"}
