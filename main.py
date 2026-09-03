42a67f05b0b1f5b9a39982483740a62cd8f085f8
Samir Azimov <samirazimov488@gmail.com>
2026-09-03 14:20:26 +0500

    added healthcheck

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+@app.get("/")
+def health_check():
+    return {"status": "http 200 ok"}
 @app.get("/users")
 def get_users():
-    return  {"users": "Ryan , Azimov"}
+    return  {"users": "Ryan , A"}
 
-@app.get("/status")
-def get_users():
-    return  {"status": "http 200 ok"}
