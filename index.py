1576714830b4023daf3ef932cd9728ef3f1cd895
Samir Azimov <samirazimov488@gmail.com>
2026-09-05 02:01:49 +0500

    added new route for 404

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
-
 from fastapi import FastAPI
-
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -15,3 +14,16 @@ def root():
 @app.get("/api/love")
 def love():
     return {"is_love": "luv u"}
+
+
+
+@app.exception_handler(404)
+def not_found(request, exc):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "error": "404 not found",
+            "message" : "The requested endpoint does not exist",
+            "path":request.url.path
+        }
+    )
