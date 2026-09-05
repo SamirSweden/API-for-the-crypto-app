1be9a2ba462b25dcbec0d10e16424244d2d64363
Samir Azimov <samirazimov488@gmail.com>
2026-09-05 21:40:26 +0500

    add new web endpoint

--- api/index.py
+++ api/index.py
@@ -18,9 +18,13 @@ app.add_middleware(
 def home():
     return {"message": "200 ok"}
 
+
+@app.get("/api/sx")
+def home():
+    return {"messageFromRyan": "I love you"}
+
 @app.get("/crypto/{coin_id}")
 async def get_price(coin_id: str):
-    # Принимает ID монеты: bitcoin, ethereum, solana и т.д.
     url = "https://api.coingecko.com/api/v3/simple/price"
 
     async with httpx.AsyncClient(timeout=5.0) as client:
@@ -29,13 +33,12 @@ async def get_price(coin_id: str):
                 url,
                 params={
                     "ids": coin_id.lower(),
-                    "vs_currencies": "usd"  # Исправлено: указан имя параметра
+                    "vs_currencies": "usd"
                 }
             )
             response.raise_for_status()
             data = response.json()
 
-            # Если передан неверный coin_id, CoinGecko возвращает {}
             if not data or coin_id.lower() not in data:
                 raise HTTPException(status_code=404, detail=f"Coin '{coin_id}' not found")
 
